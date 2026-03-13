357962d6053501f70b8dff283874cca21c0a6400
Takuya Yamamoto <eightoclockjump@gmail.com>
2026-03-14 01:36:15 +0900

    Initial Commit

---
+++ ChordToneTrainer/ChordToneTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChordToneTrainerApp.swift
+//  ChordToneTrainer
+//
+//  Created by Yamamoto Takuya on 2026/03/14.
+//
+
+import SwiftUI
+
+@main
+struct ChordToneTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
